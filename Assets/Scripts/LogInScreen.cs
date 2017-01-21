9326244695fe370c845c395c9702b37ce9633bdc
Student <student@aipdcls.edu>
2017-01-20 23:17:02 -0800

    create basic scene for launching game.

---
+++ Assets/Scripts/LogInScreen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LogInScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGame(){
+		SceneManager.LoadScene ("Level1");
+	}
+}
